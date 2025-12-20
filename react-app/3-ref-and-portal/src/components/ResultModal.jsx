a2bb2d98909cf9a5129473b15b7642e15237037c
Candi Mahendra <abdumaadjidcandi@gmail.com>
2025-12-20 22:10:16 +0700

    React module 8 - use forwardRef for React version under 19

--- react-app/3-ref-and-portal/src/components/ResultModal.jsx
+++ react-app/3-ref-and-portal/src/components/ResultModal.jsx
@@ -1,4 +1,25 @@
-export default function ResultModal({ ref, result, targetTime }) {
+import {forwardRef} from "react";
+
+// React 19 and above can use ref property directly
+// export default function ResultModal({ ref, result, targetTime }) {
+//   return (
+//     <dialog ref={ref} className={"result-modal"}>
+//       <h2>You {result}</h2>
+//       <p>
+//         The target time was <strong>{targetTime} seconds</strong>.
+//       </p>
+//       <p>
+//         You stopped the timer with <strong>... seconds left</strong>
+//       </p>
+//       <form method={"dialog"}>
+//         <button>Close</button>
+//       </form>
+//     </dialog>
+//   );
+// }
+
+// react 18 and below should use forwardRef to use ref
+const ResultModal = forwardRef(( { result, targetTime }, ref) => {
   return (
     <dialog ref={ref} className={"result-modal"}>
       <h2>You {result}</h2>
@@ -13,4 +34,6 @@ export default function ResultModal({ ref, result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+
+export default ResultModal;
