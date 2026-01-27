168aae5ac9f2a0c83ab994f711d1bfcabb0148e8
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-01-27 09:17:28 +0700

    React module 11 - when to use useEffect

--- react-app/6-use-effect/src/App.jsx
+++ react-app/6-use-effect/src/App.jsx
@@ -1,15 +1,51 @@
-import { useRef, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
+import {AVAILABLE_PLACES} from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from "./loc.js";
+
+const SELECTED_PLACES_KEY = 'selectedPlaces';
+
+function getSelectedPlacesFromStorage() {
+  return JSON.parse(localStorage.getItem(SELECTED_PLACES_KEY)) || [];
+}
+
+function setSelectedPlaceToStorage(id) {
+  const storedPlaceIds = getSelectedPlacesFromStorage();
+  if (storedPlaceIds.indexOf(id) === -1) {
+    localStorage.setItem(SELECTED_PLACES_KEY, JSON.stringify([id, ...storedPlaceIds]));
+  }
+}
+
+function removeSelectedPlace(id) {
+  const storedPlaceIds = getSelectedPlacesFromStorage();
+  localStorage.setItem(SELECTED_PLACES_KEY, JSON.stringify(storedPlaceIds.filter((id) => id !== id.current)));
+}
+
+// this code didn't require useEffect because this code can execute synchronously
+// better using useEffect for async operations with external API calls
+// also this code will execute once when the page loads, so we don't need to use useEffect for that
+const storedIds = getSelectedPlacesFromStorage();
+const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([])
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  // useEffect is good for async operations with external API calls
+  // useEffect will execute lastly after the component is rendered and after every re-render
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      const placesByDistance = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(placesByDistance);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +64,8 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    setSelectedPlaceToStorage(id);
   }
 
   function handleRemovePlace() {
@@ -35,6 +73,8 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    removeSelectedPlace(selectedPlace);
   }
 
   return (
@@ -63,7 +103,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText={'No places available at the moment.'}
           onSelectPlace={handleSelectPlace}
         />
       </main>
