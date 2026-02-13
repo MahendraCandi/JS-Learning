f5f08778a02318e6c4981ca413a17d21ffb77362
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-02-13 16:21:39 +0700

    React module 15 - add fixme

--- react-app/9-fetch-and-custom-hooks/backend/app.js
+++ react-app/9-fetch-and-custom-hooks/backend/app.js
@@ -50,4 +50,5 @@ app.use((req, res, next) => {
   res.status(404).json({ message: '404 - Not Found' });
 });
 
+console.log("run...")
 app.listen(3000);
--- react-app/9-fetch-and-custom-hooks/src/components/AvailablePlaces.jsx
+++ react-app/9-fetch-and-custom-hooks/src/components/AvailablePlaces.jsx
@@ -1,10 +1,33 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        if (!response.ok) {
+          throw new Error("Damn");
+        }
+
+        const json = await response.json();
+        console.log(json.places, 'json');
+        console.log(json);
+        setAvailablePlaces(json.places);
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    }
+    fetchPlaces();
+  }, [availablePlaces]) // fixme: why this always regenerate the component
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
