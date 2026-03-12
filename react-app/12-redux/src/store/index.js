8dd6e40a63cbb3d1b16904f3860f1c429d7e10c1
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-12 13:30:10 +0700

    React module 20 - redux toolkit createSlice

--- react-app/12-redux/src/store/index.js
+++ react-app/12-redux/src/store/index.js
@@ -1,6 +1,7 @@
 // In real project, we should use redux toolkit
 // For learning purpose, it safe to use the legacy version
 import { legacy_createStore as createStore } from 'redux';
+import {createSlice} from "@reduxjs/toolkit";
 
 // Please notice that the default value defined in the parameter.
 // Per this learn, there is no a way to define the default value through the function caller.
@@ -37,6 +38,35 @@ const reducer = (state = initiateState, action) => {
       return state;
   }
 };
+
+// The known problem could be solved by using redux toolkit
+// The createSlice will create a slice of state.
+// We can create multiple slice, and in behind redux toolkit will manage the state inside the store.
+createSlice({
+  name: 'counter', // the name of slice of state
+  initialState: initiateState, // initial value
+  reducers: {
+    // instead using action type, we can create a function for each type.
+    // This solved problem when the function become larger.
+
+    // by using object, we don't need to worry the typos.
+    // this solved problem using constant.
+    increment: (state) => {
+      // redux toolkit guarantee that the state is immutable. So we don't have to override the previous state.
+      // This solved problem the needs to always return a new state.
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    increase: (state, action) => {
+      state.count += action.increaseValue;
+    },
+    toggle: (state) => {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
 
 // Store is the main object that used by Redux to operate the state.
 // Pass the reducer function to createStore.
