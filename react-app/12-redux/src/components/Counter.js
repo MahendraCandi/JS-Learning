1051fe8607c22434d5671226d20eb25bee8c940e
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-12 11:10:58 +0700

    React module 20 - useSelector and useDispatch

--- react-app/12-redux/src/components/Counter.js
+++ react-app/12-redux/src/components/Counter.js
@@ -1,12 +1,30 @@
 import classes from './Counter.module.css';
+import {useDispatch, useSelector} from "react-redux";
 
 const Counter = () => {
+  // useSelector to get the state from the store.
+  const counter = useSelector(state => state.count);
+
+  // useDispatch to get dispatch function that used to dispatch an action.
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({type: 'increment'});
+  };
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div className={classes.operations}>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
