8ab064ab8c050e653adce6decd87aece9306f702
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-13 13:23:05 +0700

    React module 21 - Total PCS cart items

--- react-app/13-redux-practice-1/src/components/Cart/CartButton.js
+++ react-app/13-redux-practice-1/src/components/Cart/CartButton.js
@@ -1,10 +1,12 @@
 import classes from './CartButton.module.css';
+import {useSelector} from "react-redux";
 
 const CartButton = (props) => {
+  const carts = useSelector(state => state.carts.carts);
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{carts.length}</span>
     </button>
   );
 };
