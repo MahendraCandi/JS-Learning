edd6117080c45f5991da12d622302de4bc8522c8
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-16 10:54:05 +0700

    React module 21 - Implement notification

--- react-app/13-redux-practice-1/src/App.js
+++ react-app/13-redux-practice-1/src/App.js
@@ -1,29 +1,51 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
+import Notification from "./components/UI/Notification";
+import {uiActions} from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch();
   const isOpenCart = useSelector(state => state.uis.openCart);
   const carts = useSelector(state => state.carts.carts);
+  const notification = useSelector(state => state.uis.notification);
 
   // use useEffect to interact with external API, then state the changes to Redux.
   // This approach has a bug which is whenever the app start, useEffect will trigger and will send empty data to Firebase.
   // As a result, the existing data will be overwritten by the empty data.
   useEffect(() => {
-    console.log('carts changed', carts);
-    fetch('https://react-js-learning-a6947-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-      {
-        method: 'PUT',
-        body: JSON.stringify(carts),
-      })
-    .then(response => response.json())
-    .then(data => console.log('Success:', data));
-  }, [carts]);
+    console.log('useEffect - carts changed', carts);
+
+    dispatch(uiActions.pushNotification({status: 'pending', title: 'Sending...', message: 'Sending cart data'}));
+
+    const sendCarts = async () => {
+      const resp = await fetch('https://react-js-learning-a6947-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(carts),
+        });
+
+      if (!resp.ok) {
+        throw new Error('Failed to send carts');
+      }
+
+      dispatch(uiActions.pushNotification({status: 'success', title: 'Carts sent successfully', message: 'Your carts are saved.'}));
+    }
+
+    sendCarts().catch(error => {
+      dispatch(uiActions.pushNotification({status: 'error', title: 'Error!', message: error.message}));
+    });
+
+  }, [carts, dispatch]);
 
   return (
     <Layout>
+      { notification !== null &&
+        <Notification status={notification.status}
+                      title={notification.title}
+                      message={notification.message} />}
       {isOpenCart ? <Cart/> : undefined}
       <Products />
     </Layout>
--- react-app/13-redux-practice-1/src/store/ui-slice.js
+++ react-app/13-redux-practice-1/src/store/ui-slice.js
@@ -1,15 +1,30 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+/**
+ * <pre>
+ *   {
+ *     openCart: false,
+ *     notification: {
+ *       status: '',
+ *       title: '',
+ *       message: '',
+ *     },
+ *   }
+ */
 export const uiSlice = createSlice({
   name: "ui",
   initialState: {
     openCart: false,
+    notification: null,
   },
   reducers: {
     toggleCart: (state) => {
       state.openCart = !state.openCart;
       console.log('state', state.openCart);
     },
+    pushNotification: (state, payload) => {
+      state.notification = payload.payload;
+    }
   },
 });
 
