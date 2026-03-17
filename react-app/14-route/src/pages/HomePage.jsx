d2a9e194b0cd241d414af210d7bdc4764cc0a476
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 13:15:53 +0700

    React module 22 - using useNavigate

--- react-app/14-route/src/pages/HomePage.jsx
+++ react-app/14-route/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const HomePage = () => {
+
+  // useNavigate useful to create a Link programmatically.
+  const navigate = useNavigate();
   return (
     <>
       <h1>Home Page</h1>
       <p>Please visit this link <Link to={"/products"}>products</Link> </p>
+
+      {/*demonstrate useNavigate programmatically creating a Link. The real application should use Link component*/}
+      <button onClick={() => navigate("/products")}>Go to Products</button>
     </>
 
   );
