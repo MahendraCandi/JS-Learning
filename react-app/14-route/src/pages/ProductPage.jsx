fcc028086d47472f3fb907e4cfd64109f2f6ff29
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 13:41:05 +0700

    React module 22 - using dynamic path generations

--- react-app/14-route/src/pages/ProductPage.jsx
+++ react-app/14-route/src/pages/ProductPage.jsx
@@ -1,13 +1,32 @@
 import {Link} from "react-router-dom";
 
+const PRODUCTS = [
+  { id: 1, name: 'Product 1' },
+  { id: 2, name: 'Product 2' },
+  { id: 3, name: 'Product 3' },
+];
+
 const ProductPage = () => {
   return (
     <>
       <h1>Products Page</h1>
       <ul>
-        <li><Link to={'1'}>Product 1</Link></li>
-        <li><Link to={'2'}>Product 2</Link></li>
-        <li><Link to={'3'}>Product 3</Link></li>
+        {/*
+        There are two approaches to create link for path parameter:
+        1. absolute path.
+           defined full path using slash as first character followed by the path: '/products/1'
+           example: <li><Link to={'/products/1'}>Product 1</Link></li>
+        2. relative path.
+           without full path only the path parameter.
+           example: <li><Link to={'1'}>Product 1</Link></li>
+        */}
+
+        {
+          PRODUCTS.map(product => (
+            <li key={product.id}><Link to={`${product.id}`}>{product.name}</Link></li>
+          ))
+        }
+
       </ul>
     </>
   );
