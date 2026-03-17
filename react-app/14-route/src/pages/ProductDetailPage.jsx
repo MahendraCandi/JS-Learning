b3ff03b73fd595728e26a7fec67f8794c2b50263
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 14:03:54 +0700

    React module 22 - back to previous path

--- react-app/14-route/src/pages/ProductDetailPage.jsx
+++ react-app/14-route/src/pages/ProductDetailPage.jsx
@@ -1,4 +1,4 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 const ProductDetailPage = () => {
   const params = useParams();
@@ -6,6 +6,20 @@ const ProductDetailPage = () => {
     <>
       <h1>Product detail</h1>
       <p>Product ID {params.productId}</p>
+      <p>
+        {/*
+        The double dot is used to go back to the parent route.
+        Similar to Linux directory structure.
+
+        'relative' property is used to looking where link should go back.
+        If it is 'path' then will relative to the previous path.
+        Example: `/products/1` -> will be back to `/products`
+
+        If it is 'route' then will back to path and children route.
+        Example: `/products/1` -> By looking this application route setup, it will be back to `/`
+        */}
+        <Link to={'..'} relative={'path'}>Back</Link>
+      </p>
     </>
   );
 }
--- react-app/14-route/src/pages/ProductPage.jsx
+++ react-app/14-route/src/pages/ProductPage.jsx
@@ -1,9 +1,9 @@
 import {Link} from "react-router-dom";
 
 const PRODUCTS = [
-  { id: 1, name: 'Product 1' },
-  { id: 2, name: 'Product 2' },
-  { id: 3, name: 'Product 3' },
+  { id: '1', name: 'Product 1' },
+  { id: '2', name: 'Product 2' },
+  { id: '3', name: 'Product 3' },
 ];
 
 const ProductPage = () => {
