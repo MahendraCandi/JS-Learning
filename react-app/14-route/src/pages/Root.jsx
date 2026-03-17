916c6e2217d7d0f9122f30a317108dd409d5ebf9
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 11:31:40 +0700

    React module 22 - Completing navigation

--- react-app/14-route/src/pages/Root.jsx
+++ react-app/14-route/src/pages/Root.jsx
@@ -1,13 +1,17 @@
 import {Outlet} from "react-router-dom";
-import MainNavigation from "./MainNavigation";
+import MainNavigation from "../component/MainNavigation";
+import classes from "./Root.module.css";
 
 const Root = () => {
   return (
     <>
       <MainNavigation />
 
-      {/*outlet will render all children mentioned in Route creation*/}
-      <Outlet />
+      <main className={classes.content}>
+        {/*outlet will render all children mentioned in Route creation*/}
+        <Outlet />
+      </main>
+
     </>
   );
 }
