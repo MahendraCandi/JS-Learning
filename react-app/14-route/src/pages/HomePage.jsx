e373ff72ff2cc79c89b7ab7143335c84e917ae34
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 11:04:20 +0700

    React module 22 - Create a link

--- react-app/14-route/src/pages/HomePage.jsx
+++ react-app/14-route/src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
+import {Link} from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1>Home Page</h1>
+      <p>Please visit this link <Link to={"/product"}>products</Link> </p>
+    </>
+
   );
 }
 
