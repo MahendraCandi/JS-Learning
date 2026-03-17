83155d1ec37d9cf4b49042552efd92afee3155e8
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-03-17 10:34:53 +0700

    React module 22 - back to modern create route path

--- react-app/14-route/src/App.js
+++ react-app/14-route/src/App.js
@@ -1,27 +1,17 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <HomePage />,
-//   },
-//   {
-//     path: 'product',
-//     element: <ProductPage />,
-//   }
-// ]);
-
-// another way to create route path
-const routesDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="product" element={<ProductPage />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routesDefinition);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: 'product',
+    element: <ProductPage />,
+  }
+]);
 
 function App() {
   return <RouterProvider router={router} />;
