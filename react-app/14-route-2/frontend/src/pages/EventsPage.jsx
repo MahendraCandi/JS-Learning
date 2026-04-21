a9c3664fd1249c8a15bdc499bef461bd1f7fc09c
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-04-21 16:03:10 +0700

    React module 22 2nd project - Suspense and Await in a loader

--- react-app/14-route-2/frontend/src/pages/EventsPage.jsx
+++ react-app/14-route-2/frontend/src/pages/EventsPage.jsx
@@ -1,13 +1,22 @@
 import EventsList from "../components/EventsList";
-import {useLoaderData} from "react-router-dom";
+import {Await, useLoaderData} from "react-router-dom";
 import {fetchEvents} from "../utils/events-fetch";
 import LoaderResponse from "../utils/loader-response";
+import {Suspense} from "react";
 
 const EventsPage = () => {
   // useLoaderData gets data from router 'loader'.
   const response = useLoaderData();
   return (
-    <EventsList events={response.data}/>
+    <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+      <Await resolve={response.result}>
+        {
+          (loadedResponse) => {
+            return <EventsList events={loadedResponse.data}/>
+          }
+        }
+      </Await>
+    </Suspense>
   )
 }
 
@@ -17,7 +26,15 @@ export default EventsPage;
 /**
  * A loader function that fetches events from the backend.
  */
-export const loader = async () => {
-  const events = await fetchEvents(); // any error during http fetching will throw FetchException class.
-  return new LoaderResponse(events);
+export const loader = () => {
+  // by using this pattern:
+  // 1. not using async in function
+  // 2. wrap the promise in an object
+  // we can show loading fallback by using Suspense and Await components.
+  return {
+    result: fetchEvents().then((events) => {
+      // any error during http fetching will throw FetchException class.
+      return new LoaderResponse(events)
+    }),
+  };
 }
