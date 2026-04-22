bece0733986a86b9e0fa0bda081ee68fd29fe224
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-04-22 16:46:05 +0700

    React module 23 - using action to submit authentication

--- react-app/15-authentication/frontend/src/pages/Authentication.js
+++ react-app/15-authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,49 @@
 import AuthForm from '../components/AuthForm';
+import {redirect} from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action ({request}) {
+  const params = new URL(request.url).searchParams;
+  const mode = params.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw Response.json({
+      status: 500,
+      message: 'Invalid mode',
+    });
+  }
+
+  const formData = await request.formData();
+
+  const authBody = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  };
+
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authBody),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw Response.json({
+      status: 500,
+      message: 'Could not authenticate.',
+    });
+  }
+
+  // todo do something with the token!
+  return redirect('/');
+}
